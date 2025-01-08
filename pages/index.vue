de6118cb1f9930fde91f8bee03124b064b959d33
Ping1495 <ping15512@gmail.com>
2025-01-08 11:16:09 +0700

    add: nuxt content

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
       <NuxtLink to="/bmi">Go to BMI Calculator</NuxtLink>
       <NuxtLink to="/weather">Go to Weather Now</NuxtLink>
       <NuxtLink to="/xo">Go to Play tic tac toe</NuxtLink>
+      <NuxtLink to="/harryP">Go to Play Harry Potter Charecter</NuxtLink>
     </div>
   </div>
 </template>
